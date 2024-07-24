70dc18d118ecb99ff62c98f88e17c14fe30c41b4
chotepurespirit <chovyzzz.nai@gmail.com>
2024-07-24 15:22:53 +0700

    create to-do list with nodejs-express ejs

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//set template engine ejs
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended : true}));
+
+let tasks = [];
+
+app.get('/',(req,res) => {
+    res.render('index', {tasks : tasks});
+})
+
+app.post('/add', (req,res) => {
+    const newTask = req.body.task;
+    tasks.push(newTask);
+    res.redirect('/');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running successfully on ${port}`);
+});
